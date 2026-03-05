e2105218b8440a8e8ea2f49fb60eae77a29006d7
mirendarodigo <mirendarodrigo@gmail.com>
2026-03-05 15:04:35 -0300

    fix media

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -174,7 +174,7 @@ STORAGES = {
     },
 }
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ==============================================================================
