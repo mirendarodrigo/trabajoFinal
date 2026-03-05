cbb28e788b0920ff21ddc76bf3c3cecb0051ed19
mirendarodigo <mirendarodrigo@gmail.com>
2026-03-05 12:51:23 -0300

    databases

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True
     )
 }
